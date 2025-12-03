4462b72fcd74f8a38f7058d3bca73561065ea454
Wahaj Hussain <wahajhssn@gmail.com>
2025-12-03 12:48:54 -0600

    CH4: L2 JSON

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
 	"sync/atomic"
@@ -22,6 +23,7 @@ func main() {
 	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))))
 
 	mux.HandleFunc("GET /api/healthz", handlerReadiness)
+	mux.HandleFunc("POST /api/validate_chirp", handlerValidateChirp)
 
 	mux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
 	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
@@ -33,4 +35,35 @@ func main() {
 
 	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
 	log.Fatal(srv.ListenAndServe())
+}
+
+func handlerValidateChirp(w http.ResponseWriter, r *http.Request) {
+	type request struct {
+		Body string `json:"body"`
+	}
+	type response struct {
+		Valid bool   `json:"valid,omitempty"`
+		Error string `json:"error,omitempty"`
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+
+	var req request
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		res := response{Error: "Something went wrong"}
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(res)
+		return
+	}
+
+	if len(req.Body) > 140 {
+		res := response{Error: "Chirp is too long"}
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(res)
+		return
+	}
+
+	res := response{Valid: true}
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(res)
 }
