a8b143a3fdb11591d362bf6651b3b96c53d26bdc
Wahaj Hussain <wahajhssn@gmail.com>
2025-12-09 15:11:51 -0600

    CH5: L6 Create Chirp

---
+++ sql/schema/002_chirps.sql
@@ -0,0 +1,13 @@
+-- +goose Up
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
+
+CREATE TABLE chirps (
+  id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+  created_at timestamp NOT NULL DEFAULT NOW(),
+  updated_at timestamp NOT NULL DEFAULT NOW(),
+  body varchar(255) NOT NULL UNIQUE,
+  user_id uuid NOT NULL REFERENCES users(id) ON DELETE CASCADE
+);
+
+-- +goose Down
+DROP TABLE chirps;
