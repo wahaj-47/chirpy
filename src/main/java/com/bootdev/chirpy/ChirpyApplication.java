221f895ed8c5c39156c2626d4c8c0a205b066614
Wahaj Hussain <wahajhssn@gmail.com>
2026-02-15 14:48:15 -0600

    Spring Initialzr

---
+++ src/main/java/com/bootdev/chirpy/ChirpyApplication.java
@@ -0,0 +1,13 @@
+package com.bootdev.chirpy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChirpyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChirpyApplication.class, args);
+	}
+
+}
