605358bb36523309dc54cee3ca796667f966c06f
Wahaj Hussain <wahajhssn@gmail.com>
2026-02-15 14:45:21 -0600

    Java batman

--- sql/queries/refresh_tokens.sql
+++
@@ -1,9 +0,0 @@
--- name: CreateRefreshToken :one
-INSERT INTO refresh_tokens (token, user_id)
-VALUES ($1, $2)
-RETURNING *;
-
--- name: RevokeRefreshToken :exec
-UPDATE refresh_tokens
-SET revoked_at = NOW(), updated_at = NOW()
-WHERE token = $1;
--- sql/schema/003_users.sql
+++
@@ -1,7 +0,0 @@
--- +goose Up
-ALTER TABLE users
-  ADD COLUMN hashed_password varchar(255) NOT NULL DEFAULT 'unset';
-
--- +goose Down
-ALTER TABLE users
-  DROP COLUMN hashed_password;
